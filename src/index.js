e8daed940ca436bd2466c64af32f073e4af10402
usalanzhang <usalanzhang@gmail.com>
2022-02-05 15:20:22 -0800

    Add assets

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import getConfig from './config.js';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as nearAPI from 'near-api-js';
 
 // Initializing contract
