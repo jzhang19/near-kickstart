6bfa508b24d874094924a8746f9b00dbf739b4a6
usalanzhang <usalanzhang@gmail.com>
2022-02-09 18:49:09 -0800

    Update project contribution page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -4,99 +4,76 @@ import { allProjects } from "../data"
 
 
 const ProjectPage = () => {
-    const [projectData, setProjectData] = useState();
-    const history = useHistory();
+    
+    const [projectData, setProjectData] = useState({});
     const { id } = useParams();
 
-const deleteFunction = async () => {
-        // This is our API request, which we need to tell our function to wait for using the key word await
-    // await fetch(`${process.env.REACT_APP_API_URL}projects/${id}`, {
-    //       method: "delete",
-    //       headers: {
-    //         "Authorization": `Token ${localStorage.getItem('token')}`
-    //       }
-    //     })
-        // Once we delete the project above, we then want to navigate back to the homepage
-        // since the project we are looking at, doesn't exist anymore
-        history.push('/')
-      }
-
-
     useEffect(() => {
-        setProjectData(allProjects);
+        const project = allProjects.find(project => project['id'] == id);
+        console.log(project);
+        setProjectData(project);
+    });
 
-    }, );
-    const formattedDate = new Date(projectData?.date_created).toDateString()
-        // console.log(formattedDate)
-    // const deleteProject = async () => {
-    //     await fetch (`${process.env.REACT_APP_API_URL}projects/${project_id}`, {
-    //         method: "delete",
-    //         headers: {
-    //             "Authorization": `Token $`
-    //         }
-    //     })
-    // }
-    const [token, setToken] = useState(window.localStorage.getItem('token'))
-        useEffect(() => {
-            setToken(window.localStorage.getItem('token'))
-        }, [])
+    const formattedDate = new Date(projectData?.date_created).toDateString();
+    console.log(formattedDate)
+    
+    const [token, setToken] = useState(window.localStorage.getItem('token'));
+    
+    useEffect(() => {
+        setToken(window.localStorage.getItem('token'))
+    }, []);
 
     const newDonation = () => {
         localStorage.getItem('token')
         return (
             <div>
-            {
-                token || token !== "null"
-                ? history.push('/donation-form')
-                : <div></div>
-    }
+                {
+                    token || token !== "null"
+                    ? history.push('/donation-form')
+                    : <div></div>
+                }
             </div>
-        )}
+    )};
 
     return  (
         <React.Fragment>
             <section className="text-block mb-70 text-center">
-                <h4>Checkout</h4>
-                <p><i className="fa fa-lock" aria-hidden="true"></i>This page uses 256-bit encryption. Your payment data is never stored locally.</p>
+                <h4>CrowdLoan</h4>
             </section>
             <section className="features border-decor-parent mb-60">
                 <i className="btn-close"></i>
                 <div className="row">
                     <div className="col-md-4 col-sm-4 text-center">
-                        <img src="images/eiefy_0056_Product-2.png" alt=""/>
+                        <img src={projectData.image} alt=""/>
                     </div>
                     <div className="col-md-8 col-sm-8">
                         <div className="features-info">
-                            <h6 className="border-decor">Geo Shop - Groceries Shopping Website</h6>
+                            <h6 className="border-decor">{projectData.title}</h6>
                             <table className="table">
                                 <tbody>
                                     <tr>
-                                        <td>Support</td>
+                                        <td>Description</td>
+                                        <td>{projectData.description}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lock Period</td>
                                         <td>12 Months</td>
                                     </tr>
                                     <tr>
-                                        <td>Updates</td>
-                                        <td>Free Lifetime</td>
+                                        <td>Goal</td>
+                                        <td>{projectData.goal}</td>
                                     </tr>
                                     <tr>
-                                        <td>Domain Usage</td>
-                                        <td>Unlimited</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Included files</td>
+                                        <td>Accepted Tokens</td>
                                         <td>
                                             <div className="tags">
-                                                <a href="#">psd</a>
-                                                <a href="#">css</a>
-                                                <a href="#">php</a>
-                                                <a href="#">js</a>
-                                                <a href="#">icons</a>
+                                                <a href="#">BTC</a>
+                                                <a href="#">ETH</a>
+                                                <a href="#">NEAR</a>
+                                                <a href="#">DOT</a>
+                                                <a href="#">ATOM</a>
                                             </div>
                                         </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Refund Policy</td>
-                                        <td>30 Days, no-question asked</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -106,7 +83,7 @@ const deleteFunction = async () => {
             </section>
             <section className="cart-block row mb-40">
                 <div className="col-md-8">
-                    <h5>Add additional service</h5>
+                    <h5>Contribute</h5>
                     <div className="payment mb-30">
                         <div className="payment-method">
                             <div className="active"><img src="images/eiefy_0019_Payment_01.png" alt=""/></div>
@@ -114,63 +91,42 @@ const deleteFunction = async () => {
                             <div><img src="images/eiefy_0017_Payment_03.png" alt=""/></div>
                         </div>
                         <div className="payment-info">
-                            <label>Name on Card</label>
+                            <label>Current NEAR</label>
                             <input type="text" className="form-control"/>
-                            <label>Card Number</label>
+                            <label>Contribute Amount</label>
                             <input type="text" className="form-control"/>
                             <div className="row inline-select">
-                                <div className="col-md-7">
-                                    <label>Expiry Date</label>
-                                    <select name="expiryD" className="form-control">
-                                        <option>MM</option>
-                                        <option>Option #1</option>
-                                        <option>Option #2</option>
-                                    </select>
-                                    <span>/</span>
-                                    <select name="expiryY" className="form-control">
-                                        <option>YY</option>
-                                        <option>Option #1</option>
-                                        <option>Option #2</option>
-                                    </select>
-                                </div>
-                                <div className="col-md-5">
-                                    <label>CVV</label>
+                                <div className="col-md-12">
+                                    <label>Email to be notified</label>
                                     <input type="text" className="form-control" placeholder="Add"/>
                                 </div>
                             </div>
-                            <label className="checkbox-text">
-                                <label className="checkbox-default">
-                                    <input type="checkbox"/>
-                                    <span className="arrow"></span>
-                                </label>
-                                <p>Save card for next time</p>
-                            </label>
                             <table className="table mt-30">
                                 <tbody>
                                     <tr className="total">
-                                        <td>Your Order Total</td>
+                                        <td>Your contribution amount</td>
                                         <td>$101.00</td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <a href="#" className="btn btn-small">Make Payment</a>
+                            <a href="#" className="btn btn-small">Contribute</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <h5>Order Summary</h5>
+                    <h5>Rewards Summary</h5>
                     <table className="table mb-30">
                         <tbody>
                             <tr>
-                                <td>Geo shop - groceries ..</td>
+                                <td>Base Rewards</td>
                                 <td>$39.00</td>
                             </tr>
                             <tr>
-                                <td>Handling Fee:</td>
+                                <td>Early Bird Bonus</td>
                                 <td>$2.00</td>
                             </tr>
                             <tr>
-                                <td>Email Newsletter</td>
+                                <td>Referal Bonus</td>
                                 <td>$60.00</td>
                             </tr>
                             <tr className="total">
@@ -181,45 +137,8 @@ const deleteFunction = async () => {
                     </table>
                 </div>
             </section>
-            <div id="card">
-                <div>
-                    <h2>{projectData?.title}</h2>
-                    <h3>Location: {projectData?.location}</h3>
-                    <h3>Created on: {formattedDate}</h3>
-                    <h3>{projectData?.description}</h3>
-                    <h3>Goal: $ {projectData?.goal}.00</h3>
-                    <h3>Raised so far: $ {projectData?.total_donated}.00</h3>
-                    <button id="delete-button" className="submit-button" onClick={newDonation}>Donate!</button>
-                    {/* <button id="delete-button" className="submit-button" onClick>Donate!</button> */}
-                    <img alt="project" src= {projectData?.image}/>
-                </div>
-                {token !== "null"
-                ?<div>        
-                    {/* <button onClick={editFunction}>Edit Project</button> */}
-                    <button id="delete-button" className="submit-button" onClick={deleteFunction}>Delete Project</button>
-                </div>
-                : <div></div>
-                }
-            </div>
         </React.Fragment>
     )
 }
 
-
 export default ProjectPage
-
-// date_created: "2020-03-21T00:28:23.382748+10:00"
-// description: "Another project"
-// goal: 1000
-// id: 4
-// image: "https://via.placeholder.com/300.jpg"
-// is_open: true
-// location: "STAFFORD"
-// owner: 11
-// title: "Project"
-
-// const quickFunc = (someVar) => {
-//     if(someVar && someVar.amount) {
-//         // do stuff
-//     }
-// }
